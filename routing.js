ff8ea4aa58883068383e6b8ed16e1ee2232dfeb6
imthiyazhusaina <imthiyazhusaina@gmail.com>
2026-07-23 22:26:57 +0530

    Routing

--- routing.js
+++ routing.js
@@ -9,6 +9,10 @@ app.get('/about',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
 })
 
+app.get('*',(req,res)=>{
+    res.status(404).send('<h1>404 Not Found</h1><p>The page you are looking for does not exist.</p>');
+});
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 })
